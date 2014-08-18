6b82c7a1164ce28fdc0afae98971d7e2b9ae74d4
emptymalei <emptymalei@qq.com>
2014-08-18 11:09:13 -0600

    mod conf.py to eliminate theme settings

--- conf.py
+++ conf.py
@@ -238,7 +238,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme = 'haiku'
-html_theme = 'bootstrap'
+# html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -251,7 +251,7 @@ html_theme = 'bootstrap'
 
 
 # For bootstrap theme:
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # bootswatch_theme = "cosmo"
 
 
